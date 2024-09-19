04c44b40564357f206494b7a9194a2ae06cc3a90
jaygupta19 <jayajaygupta19@gmail.com>
2024-09-19 12:56:53 +0000

    Fixed navbar , active page , Basic css fixes of register,resources , about and home page

--- src/Components/Ham.js
+++ src/Components/Ham.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
 // import ReactDOM from 'react-dom/client';
 import Style from "../Stylesheets/Ham.module.css"
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 import { Menu , X } from "lucide-react";
 const Ham = () => {
+    const path = useLocation()
     const [isopen , setIsOpen] = useState(false);
-        const toggleHam = () => {
+    const toggleHam = () => {
             setIsOpen((prev)=>!prev);
     }
     return (
         <>
             <div className={Style.hamBtnContainer}>
                 <div className={Style.hamBtn} onClick={toggleHam} >
-                   {!isopen? <Menu color='white'/> : <X color='white'/>}
+                {!isopen && <Menu color='white'/>}
                 </div>
             </div>
             {isopen && <div className={Style.hamContainer}>
-                <nav >
-                    <ul className={Style.list}>
-                        <li><Link to={"/"} className={Style.active}>Home</Link></li>
-                        <li><Link to={"/about"}>About</Link></li>
-                        <li><Link to={"/achievements"}>Achievements</Link></li>
-                        <li><Link to={"/events"}>Events</Link></li>
-                        <li><Link to={"/resources"}> Resources </Link></li>
-                    </ul>
+                <div className={Style.hamBtn} onClick={toggleHam} >
+                   {isopen && <X color='white'/>}
+                </div>
+                <nav>
+                <ul className={Style.list}>
+                        <li><Link onClick={toggleHam} to={"/"} className={path.pathname=="/" ? Style.active : ""}>Home</Link></li>
+                        <li><Link className={path.pathname=="/about" ? Style.active : ""} onClick={toggleHam} to={"/about"}>About</Link></li>
+                        <li><Link className={path.pathname=="/achievements" ? Style.active : ""} onClick={toggleHam} to={"/achievements"}>Achievements</Link></li>
+                        <li><Link className={path.pathname=="/events" ? Style.active : ""} onClick={toggleHam} to={"/events"}>Events</Link></li>
+                        <li><Link className={path.pathname=="/resources" ? Style.active : ""} onClick={toggleHam} to={"/resources"}> Resources </Link></li>
+                </ul>
                 </nav>
             </div>}
             

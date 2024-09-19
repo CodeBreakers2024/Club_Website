a5d1e152ac7f5b05a19211f7ae6e632b999831b6
Xyncross1111 <anasmk2003@gmail.com>
2024-09-20 01:11:20 +0530

    update: registration form

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../Stylesheets/Registration.module.css";
 import upi from "../Assets/UPI.jpg";
 import { useNavigate } from "react-router-dom";
@@ -17,10 +17,12 @@ const firebaseConfig = {
 };
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 }
 
 const contactFormDB = firebase.database().ref("registrationForm");
+
+const members = 3;
 
 const RegistrationForm = () => {
     const navigate = useNavigate();
@@ -33,42 +35,55 @@ const RegistrationForm = () => {
             'contact': '',
             'year': ''
         },
-        'members': [
-            {
-                'name': '',
-                'email': ''
-            },
-            {
-                'name': '',
-                'email': ''
-            },
-            {
-                'name': '',
-                'email': ''
-            }
-        ],
+        'members': [],
         'payment': {
             'transactionId': ''
         }
-    });
+    })
+
+    useEffect(() => {
+
+        let memberlist = [];
+
+        for (let i = 0; i < members; i++) {
+            memberlist.push({
+                'name': '',
+                'email': ''
+            })
+        }
+
+        setTeamData(prev => ({
+            ...prev,
+            members: memberlist
+        }))
+    }, [])
 
     const [page, setPage] = useState(0);
 
     const handleFormSwitch = (e) => {
-        if (e.target.name === "next") setPage((prev) => prev + 1);
-        else if (e.target.name === "prev") setPage((prev) => prev - 1);
-    };
+        if (e.target.name === "next") setPage((prev) => prev + 1)
+        else if (e.target.name === "prev") setPage((prev) => prev - 1)
+    }
 
-    const handleLeaderChange = (e) => {
+    const handleTeamChange = (e) => {
         const { name, value } = e.target;
-        setTeamData((prev) => ({
+
+        if (name === "teamName") {
+            setTeamData((prev) => ({
+                ...prev,
+                [name]: value
+            }))
+        }
+
+        else setTeamData((prev) => ({
             ...prev,
             leader: {
                 ...prev.leader,
-                [name]: value,
+                [name]: value
             },
         }));
-    };
+
+    }
 
     const handleMemberChange = (e) => {
         const { name, value } = e.target;
@@ -77,10 +92,10 @@ const RegistrationForm = () => {
             updatedMembers[page - 1] = { ...updatedMembers[page - 1], [name]: value };
             return {
                 ...prev,
-                members: updatedMembers,
+                members: updatedMembers
             };
         });
-    };
+    }
 
     const handlePaymentChange = (e) => {
         const { name, value } = e.target;
@@ -91,7 +106,7 @@ const RegistrationForm = () => {
                 [name]: value,
             },
         }));
-    };
+    }
 
     const saveMessages = (teamData) => {
         const newContactForm = contactFormDB.push();
@@ -100,7 +115,7 @@ const RegistrationForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
         const teamNameSnapshot = await contactFormDB.orderByChild("leader/teamName").equalTo(teamData.teamName).once("value");
         if (teamNameSnapshot.exists()) {
             alert("Team name already exists!");
@@ -118,7 +133,7 @@ const RegistrationForm = () => {
             alert("Leader's contact number already exists!");
             return;
         }
-    
+
         for (let i = 0; i < teamData.members.length; i++) {
             const memberEmailSnapshot = await contactFormDB.orderByChild(`members/${i}/email`).equalTo(teamData.members[i].email).once("value");
             if (memberEmailSnapshot.exists()) {
@@ -126,138 +141,146 @@ const RegistrationForm = () => {
                 return;
             }
         }
-    
+
         saveMessages(teamData);
-    
+
         alert("Form submitted successfully!");
         navigate("/");
     };
-    
     return (
-        <>
         <div className={styles.registrationContainer}>
-           <h1 className={styles.title}>Registration Form</h1>
-           <div className={styles.formContainer}>
-               {page === 0 && <TeamInfoForm handleChange={handleLeaderChange} />}
-               {page === 1 && <MemberInfoForm handleChange={handleMemberChange} />}
-               {page === 2 && <MemberInfoForm handleChange={handleMemberChange} />}
-               {page === 3 && <MemberInfoForm handleChange={handleMemberChange} />}
-               {page === 4 && <PaymentInfoForm handleChange={handlePaymentChange} />}
+            <h1 className={styles.title}>Registration Form</h1>
+            <div className={styles.formContainer} onSubmit={handleSubmit}>
 
-               <div className={styles.buttonGroup}>
-                   <button 
-                       className={styles.prevButton} 
-                       name="prev" 
-                       onClick={handleFormSwitch} 
-                       disabled={page === 0}>
-                       Previous
-                   </button>
+                {page === 0 && <TeamInfoForm teamData={teamData} handleChange={handleTeamChange} />}
 
-                   <button 
-                       className={styles.nextButton} 
-                       name="next" 
-                       onClick={handleFormSwitch} 
-                       disabled={page === 4}>
-                       Next
-                   </button>
+                {page > 0 && page <= members && (
+                    <MemberInfoForm page={page} memberlist={teamData.members} handleChange={handleMemberChange} />
+                )}
 
-                   <button 
-                       className={styles.submitButton} 
-                       type="submit" 
-                       onClick={handleSubmit}
-                       disabled={page !== 4}>
-                       Submit
-                   </button>
-               </div>
-           </div>
+                {page === members + 1 && <PaymentInfoForm payment={teamData.payment} handleChange={handlePaymentChange} />}
+
+                <div className={styles.buttonGroup}>
+                    <button
+                        className={styles.prevButton}
+                        name="prev"
+                        onClick={handleFormSwitch}
+                        disabled={page === 0}>
+                        Previous
+                    </button>
+
+                    <button
+                        className={styles.nextButton}
+                        name="next"
+                        onClick={handleFormSwitch}
+                        disabled={page === members + 1}>
+                        Next
+                    </button>
+
+                    <button
+                        className={styles.submitButton}
+                        type="submit"
+                        disabled={page !== members + 1}
+                        onClick={handleSubmit} >
+                        Submit
+                    </button>
+                </div>
+            </div>
         </div>
-        </>
-    );
+    )
 };
 
-const TeamInfoForm = ({ handleChange }) => {
+const TeamInfoForm = ({ teamData, handleChange }) => {
     return (
         <div className={styles.teamInfoForm}>
             <label className={styles.formLabel}>Team Name:</label>
-            <input 
-                type="text" 
-                placeholder="Team Name" 
-                name="teamName" 
-                onChange={handleChange} 
-                className={styles.formInput} 
+            <input
+                type="text"
+                placeholder="Team Name"
+                name="teamName"
+                value={teamData.teamName}
+                onChange={handleChange}
+                className={styles.formInput}
             />
 
             <label className={styles.formLabel}>Leader Details</label>
             <label className={styles.formLabel}>Name:</label>
-            <input 
-                type="text" 
-                placeholder="Leader Name" 
-                name="name" 
-                onChange={handleChange} 
-                className={styles.formInput} 
+            <input
+                type="text"
+                placeholder="Leader Name"
+                name="name"
+                value={teamData.leader.name}
+                onChange={handleChange}
+                className={styles.formInput}
             />
 
             <label className={styles.formLabel}>Email:</label>
-            <input 
-                type="email" 
-                placeholder="Email" 
-                name="email" 
-                onChange={handleChange} 
-                className={styles.formInput} 
+            <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={teamData.leader.email}
+                onChange={handleChange}
+                className={styles.formInput}
             />
 
             <label className={styles.formLabel}>Contact:</label>
-            <input 
-                type="phone" 
-                placeholder="Contact" 
-                name="contact" 
-                onChange={handleChange} 
-                className={styles.formInput} 
+            <input
+                type="phone"
+                placeholder="Contact"
+                name="contact"
+                value={teamData.leader.contact}
+                onChange={handleChange}
+                className={styles.formInput}
             />
         </div>
-    );
-};
+    )
+}
 
-const MemberInfoForm = ({ handleChange }) => {
+const MemberInfoForm = ({ page, memberlist, handleChange }) => {
     return (
         <div className={styles.memberInfoForm}>
+            <label className={styles.formLabel}>Member {page} </label>
             <label className={styles.formLabel}>Member Name:</label>
-            <input 
-                type="text" 
-                placeholder="Member Name" 
-                name="name" 
-                onChange={handleChange} 
-                className={styles.formInput} 
+            <input
+                type="text"
+                placeholder="Member Name"
+                name="name"
+                value={memberlist[page - 1].name}
+                onChange={handleChange}
+                className={styles.formInput}
             />
 
             <label className={styles.formLabel}>Member Email:</label>
-            <input 
-                type="email" 
-                placeholder="Member Email" 
-                name="email" 
-                onChange={handleChange} 
-                className={styles.formInput} 
+            <input
+                type="email"
+                placeholder="Member Email"
+                name="email"
+                value={memberlist[page - 1].email}
+                onChange={handleChange}
+                className={styles.formInput}
             />
         </div>
-    );
-};
+    )
+}
 
-const PaymentInfoForm = ({ handleChange }) => {
+const PaymentInfoForm = ({ payment, handleChange }) => {
     return (
         <div className={styles.paymentInfoForm}>
             <label className={styles.formLabel}>Account Details:</label>
             <img src={upi} alt="UPI QR Code" className={styles.upiImage} />
 
             <label className={styles.formLabel}>Transaction ID:</label>
-            <input 
-                type="text" 
-                placeholder="Transaction ID" 
-                name="transactionId" 
-                onChange={handleChange} 
-                className={styles.formInput} 
+            <input
+                type="text"
+                placeholder="Transaction ID"
+                name="transactionId"
+                value={payment.transactionId}
+                onChange={handleChange}
+                className={styles.formInput}
             />
         </div>
-    );
-};
+    )
+}
 
 export default RegistrationForm;

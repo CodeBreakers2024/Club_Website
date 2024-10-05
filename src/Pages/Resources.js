4bfd11f8243f053a5f67801f32a9a293bf9a1b19
Mandar <mandargurjar51@gmail.com>
2024-10-05 20:04:02 +0530

    Added new card trigger at 1000px

--- src/Pages/Resources.js
+++ src/Pages/Resources.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Rbottom from "../Components/Rbottom";
 import Rtop from '../Components/Rrtop.js';
+import MobileRtop from "../Components/MobileRtop.js";
 
 function Resources() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  
+  const handleResize = () => {
+    if (window.innerWidth <= 1000) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  
+  useEffect(() => {
+    handleResize(); 
+    window.addEventListener("resize", handleResize); 
+
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-      <Rtop />
+      {isMobile ? <MobileRtop /> : <Rtop />}
       <Rbottom />
     </>
   );

3d2642e14ce7c15afa7cdc81f23664cce8862f34
CodeBreakers2024 <163032394+CodeBreakers2024@users.noreply.github.com>
2024-10-09 14:49:08 +0530

    Update PgAbout.js

--- src/Components/PgAbout.js
+++ src/Components/PgAbout.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState, useRef } from "react";
 import Style from "../Stylesheets/Pgabout.module.css";
-import Domain from './Domain'
+import Domain from './Domain';
 import domains from "../data/Members.json";
 import useTypingEffect from "./typinghook";
 
 function PgAbout() {
-  // const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
   const [startTyping, setStartTyping] = useState(false);
-  const typedText = useTypingEffect("About Us", 150, startTyping); // Modified to accept startTyping
-  const typedText1 = useTypingEffect("From preschool to pre-tertiary, our students enjoy fun, interactive and relevant lessons and are empowered to think beyond the confines of the classroom.", 10, startTyping); 
+  const typedText = useTypingEffect("About Us", 150, startTyping);
+  const typedText1 = useTypingEffect(
+    "From preschool to pre-tertiary, our students enjoy fun, interactive and relevant lessons and are empowered to think beyond the confines of the classroom.",
+    10,
+    startTyping
+  );
   const scrollContainerRef = useRef(null);
 
   const handleScroll = () => {
@@ -36,27 +40,28 @@ function PgAbout() {
         scrollContainer.removeEventListener('scroll', handleScroll);
       }
     };
-  }, []);
+  }, [handleScroll]); // Added handleScroll to the dependency array
 
   return (
     <div 
       ref={scrollContainerRef}
       style={{
-        minHeight:"100vh",
-        width:"100%",
+        minHeight: "100vh",
+        width: "100%",
         overflowY: "auto",
-        // overflowX:"hidden"
       }}
     >
       <div className={Style.contianer}>
         <div className={Style.h2}>{typedText}</div>
         <div className={Style.para}>
-         {typedText1}
+          {typedText1}
         </div>
       </div>
-      {domains.map((domain, i) => {
-        return <Domain key={i} domain={domain} />;
-      })}
+      <div className={Style.domains}>
+        {domains.map((domain, i) => (
+          <Domain key={i} domain={domain} />
+        ))}
+      </div>
     </div>
   );
 }

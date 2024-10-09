3bfeccc9bd819a6ae3c32edf6b1c3e992f970f98
CodeBreakers2024 <163032394+CodeBreakers2024@users.noreply.github.com>
2024-10-09 14:59:03 +0530

    Update Homepage.js

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Carousel from "../Components/Carousel";
+// import Carousel from "../Components/Carousel";
 import Cards from "../Components/Cards";
 import Domains from '../Components/Domains'
 import BottomCarousel from '../Components/BottomCarousel';

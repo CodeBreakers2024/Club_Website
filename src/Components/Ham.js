a0ed5f607bf4644c2aeebc612639d5e0e63a5ba2
CodeBreakers2024 <163032394+CodeBreakers2024@users.noreply.github.com>
2024-10-09 14:45:56 +0530

    Update Ham.js

--- src/Components/Ham.js
+++ src/Components/Ham.js
@@ -23,10 +23,10 @@ const Ham = () => {
                 <nav>
                 <ul className={Style.list}>
                         <li><Link onClick={toggleHam} to={"/"} className={path.pathname=="/" ? Style.active : ""}>Home</Link></li>
-                        <li><Link className={path.pathname=="/about" ? Style.active : ""} onClick={toggleHam} to={"/about"}>About</Link></li>
-                        <li><Link className={path.pathname=="/achievements" ? Style.active : ""} onClick={toggleHam} to={"/achievements"}>Achievements</Link></li>
-                        <li><Link className={path.pathname=="/events" ? Style.active : ""} onClick={toggleHam} to={"/events"}>Events</Link></li>
-                        <li><Link className={path.pathname=="/resources" ? Style.active : ""} onClick={toggleHam} to={"/resources"}> Resources </Link></li>
+                        <li><Link className={path.pathname==="/about" ? Style.active : ""} onClick={toggleHam} to={"/about"}>About</Link></li>
+                        <li><Link className={path.pathname==="/achievements" ? Style.active : ""} onClick={toggleHam} to={"/achievements"}>Achievements</Link></li>
+                        <li><Link className={path.pathname==="/events" ? Style.active : ""} onClick={toggleHam} to={"/events"}>Events</Link></li>
+                        <li><Link className={path.pathname==="/resources" ? Style.active : ""} onClick={toggleHam} to={"/resources"}> Resources </Link></li>
                 </ul>
                 </nav>
             </div>}
